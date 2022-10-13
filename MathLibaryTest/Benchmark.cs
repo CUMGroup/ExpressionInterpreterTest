6ebb96945180cea3ef323c3d10de07860093a78a
Alex Hagl <alex@hagl.de>
2022-10-13 21:53:36 +0200

    Benchmarks

--- MathLibaryTest/Benchmark.cs
+++ MathLibaryTest/Benchmark.cs
@@ -9,12 +9,28 @@ using System.Threading.Tasks;
 namespace MathLibaryTest {
     public class Benchmark {
 
+        [Benchmark]
+        public Entity CheckEqualityHard() {
+
+            Entity expr = "(x+13)^2*(x-2.72)*(x+1.12)*x^2*(x-14)";
+            //expr = expr.Expand();
+            Entity expr1 = "x^7 + 10.4*x^6 - 217.246*x^5 - 2090.56*x^4 + 4379.65*x^3 + 7207.78*x^2";
+            var res = new Entity.Equalsf(expr, expr1).Simplify();
+            return res;
+        }
 
         [Benchmark]
-        public void CheckEquality() {
-            Entity expr = "(x+13)^2*(x-2.72)*(x+1.12)*x^2*(x-14)";
-            Entity expr1 = "x^7 + 10.4*x^6 - 217.246*x^5 - 2090.56*x^4 + 4379.65*x^3 + 7207.78*x^2";
-            Console.WriteLine(new Entity.Equalsf(expr, expr1).Simplify());
+        public Entity CheckEqualityEasy() {
+            Entity expr = "x^5-27*x^4+7.5*x^3-12*x^2+x-2.72";
+            Entity expr1 = "x^5-27*x^4+7.5*x^3-12*x^2+x-2.72";
+            var res = new Entity.Equalsf(expr, expr1).Simplify();
+            return res;
+        }
+
+        [Benchmark]
+        public Entity Calculate() {
+            Entity expr = "x^5-27*x^4+7.5*x^3-12*x^2+x-2.72";
+            return expr.Substitute("x", Random.Shared.NextDouble() * 1000 - 500);
         }
 
     }
--- MathLibaryTest/Program.cs
+++ MathLibaryTest/Program.cs
@@ -3,6 +3,5 @@ using BenchmarkDotNet.Running;
 using MathLibaryTest;
 
 //BenchmarkRunner.Run<Benchmark>();
-Entity expr = "(x+13)^2*(x-2.72)*(x+1.12)*x^2*(x-14)";
-Entity expr1 = "(x+13)^2*(x-2.72)*(x+1.12)*x^2*(x-14)";
-Console.WriteLine(new Entity.Equalsf(expr, expr1).Simplify());
+Entity expr = "x^5-27*x^4+7.5*x^3-12*x^2+x-2.72";
+Console.WriteLine(expr.Substitute("x", 1_000_000_000_000_000_000).Simplify());
